96e331f9f84986840252470c1c45cbdde5a48536
KillianGauchet <killian.gauchet-boucher@etu.iut-tlse3.fr>
2026-05-24 03:49:35 +0200

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createContext, useContext, useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence, useInView } from 'framer-motion';
 import chadImg from './assets/chad.jpg';
 import codenomiconImg from './assets/Codenomicon.jpg';
